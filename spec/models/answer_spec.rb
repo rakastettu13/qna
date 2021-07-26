a987c7eda7a44e7d717a4ebd574c26a3e74d420b
Marina <rakastettu13@gmail.com>
2021-07-26 07:39:07 +0300

    Answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Question, type: :model do
+RSpec.describe Answer, type: :model do
   context 'with associations' do
-    it { is_expected.to have_many :answers }
+    it { is_expected.to belong_to :question }
   end
 
   context 'with validations' do
-    it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :body }
   end
 end
