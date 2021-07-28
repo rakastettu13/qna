ed117a6027f0554e3b98baca021c266e482e1bd6
Marina <rakastettu13@gmail.com>
2021-07-28 12:20:35 +0300

    new, create actions for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i[show new create]
+  resources :questions, only: %i[show new create] do
+    resources :answers, only: %i[show new create], shallow: true
+  end
 end
