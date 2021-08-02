08e9b52abbe250fc7e8c2607f7b218106fe042bb
Marina <rakastettu13@gmail.com>
2021-08-02 06:06:37 +0300

    gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'questions#index'
+
   resources :questions, only: %i[index show new create] do
     resources :answers, only: %i[create], shallow: true
   end
