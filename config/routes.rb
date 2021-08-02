7ad7172660a2d6801b137244ae8660d5db170d41
Marina <rakastettu13@gmail.com>
2021-08-02 07:54:54 +0300

    user can sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
 
+  devise_for :users
+
   resources :questions, only: %i[index show new create] do
     resources :answers, only: %i[create], shallow: true
   end
