7f338a86200c95986df632f4272c04f873270963
Marina <rakastettu13@gmail.com>
2021-08-06 13:19:22 +0300

    User#author_of? spec refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :questions, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
   has_many :answers, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
 
-  def author_of?(something)
-    something.author_id == id
+  def author_of?(resource)
+    resource.author_id == id
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,17 +9,21 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:answers).dependent(:destroy) }
   end
 
-  describe 'have method author_of?' do
-    let(:user) { create(:user) }
-    let(:question) { create(:question, author: user) }
-    let(:another_user) { create(:user) }
+  describe '#author_of?' do
+    subject { user }
 
-    it 'user is the author' do
-      expect(user).to be_author_of question
+    let(:user) { create(:user) }
+
+    context 'when user is the author' do
+      let(:resource) { create(:question, author: user) }
+
+      it { is_expected.to be_author_of(resource) }
     end
 
-    it 'user is not the author' do
-      expect(another_user).not_to be_author_of question
+    context 'when user is not the author' do
+      let(:resource) { create(:question) }
+
+      it { is_expected.not_to be_author_of(resource) }
     end
   end
 end
