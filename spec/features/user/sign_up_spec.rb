ac4a3603580e806dac05cb6d0245261a0136d549
Marina <rakastettu13@gmail.com>
2021-08-22 08:58:01 +0300

    sign_up_spec refactoring

--- spec/features/user/sign_up_spec.rb
+++ spec/features/user/sign_up_spec.rb
@@ -1,29 +1,27 @@
 require 'rails_helper'
 
 feature 'User can sign up' do
-  describe 'User tries to register' do
-    background { visit new_user_registration_path }
+  background { visit new_user_registration_path }
 
-    scenario 'with valid email and password' do
-      fill_in 'Email', with: 'my@string.com'
-      fill_in 'Password', with: 'MyString'
-      fill_in 'Password confirmation', with: 'MyString'
+  scenario 'with valid email and password' do
+    fill_in 'Email', with: 'my@string.com'
+    fill_in 'Password', with: 'MyString'
+    fill_in 'Password confirmation', with: 'MyString'
 
-      within('.actions') { click_on 'Sign up' }
+    within('.actions') { click_on 'Sign up' }
 
-      expect(page).to have_content 'Welcome! You have signed up successfully.'
-      expect(find('.user_links')).to have_content 'Log out'
-    end
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(find('.user_links')).to have_content 'Log out'
+  end
 
-    scenario 'with invalid email and password' do
-      within('.actions') { click_on 'Sign up' }
+  scenario 'with invalid email and password' do
+    within('.actions') { click_on 'Sign up' }
 
-      expect(page).to have_content 'errors prohibited this user from being saved:'
+    expect(page).to have_content 'errors prohibited this user from being saved:'
 
-      within('.user_links') do
-        expect(page).to have_content 'Log in'
-        expect(page).to have_content 'Sign up'
-      end
+    within('.user_links') do
+      expect(page).to have_content 'Log in'
+      expect(page).to have_content 'Sign up'
     end
   end
 end
