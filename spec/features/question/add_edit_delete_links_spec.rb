a0420cb1b3e49692e67512338c144edfa04eeba0
Marina <rakastettu13@gmail.com>
2021-08-24 05:12:15 +0300

    user can add several links when creating a question

--- spec/features/question/add_edit_delete_links_spec.rb
+++ spec/features/question/add_edit_delete_links_spec.rb
@@ -4,8 +4,9 @@ RSpec.feature 'The user can attach links when creating and updating a question, 
               type: :feature do
   given(:user) { create(:user) }
   given(:qna_url) { 'https://github.com/rakastettu13/qna' }
+  given(:gist_url) { 'https://github.com/rakastettu13/qna' }
 
-  scenario 'The user can attach link when creating a question' do
+  scenario 'The user can attach links when creating a question', js: true do
     sign_in(user)
     visit new_question_path
 
@@ -15,8 +16,16 @@ RSpec.feature 'The user can attach links when creating and updating a question, 
     fill_in 'Name', with: 'qna link'
     fill_in 'Url', with: qna_url
 
+    click_on 'add link'
+
+    within all('.nested-fields')[1] do
+      fill_in 'Name', with: 'gist link'
+      fill_in 'Url', with: gist_url
+    end
+
     click_on 'Ask'
 
     expect(find('.question-links')).to have_link 'qna link', href: qna_url
+    expect(find('.question-links')).to have_link 'gist link', href: gist_url
   end
 end
