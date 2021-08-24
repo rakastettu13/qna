f4aa8445419682bc7020d6059ff9b0cdd8ab6423
Marina <rakastettu13@gmail.com>
2021-08-24 07:33:00 +0300

    author can add a link when editing a question

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
   factory :link do
     name { 'MyString' }
-    url { 'MyString' }
-    linkable
+    url { 'https://google.com' }
+
+    trait :for_question do
+      association :linkable, factory: :question
+    end
+
+    trait :for_answer do
+      association :linkable, factory: :answer
+    end
   end
 end
--- spec/features/question/add_edit_delete_links_spec.rb
+++ spec/features/question/add_edit_delete_links_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature 'The user can attach links when creating and updating a question, edit and delete attached links',
+RSpec.feature 'The user can attach links when creating and updating a question, edit and delete attached links.',
               type: :feature do
   given(:user) { create(:user) }
   given(:qna_url) { 'https://github.com/rakastettu13/qna' }
   given(:gist_url) { 'https://github.com/rakastettu13/qna' }
+  given(:question) { create(:question, author: user) }
+  given!(:link) { create(:link, linkable: question) }
 
-  scenario 'The user can attach links when creating a question', js: true do
-    sign_in(user)
+  before { sign_in(user) }
+
+  scenario 'The user tries to attach links when creating a question', js: true do
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
@@ -18,14 +21,34 @@ RSpec.feature 'The user can attach links when creating and updating a question, 
 
     click_on 'add link'
 
-    within all('.nested-fields')[1] do
+    within all('.nested-fields').last do
       fill_in 'Name', with: 'gist link'
       fill_in 'Url', with: gist_url
     end
 
     click_on 'Ask'
 
-    expect(find('.question-links')).to have_link 'qna link', href: qna_url
-    expect(find('.question-links')).to have_link 'gist link', href: gist_url
+    within('.question-links') do
+      expect(page).to have_link 'qna link', href: qna_url
+      expect(page).to have_link 'gist link', href: gist_url
+    end
+  end
+
+  scenario 'Author tries to add link when editing a question', js: true do
+    visit question_path(question)
+    click_on 'Edit the question'
+    click_on 'add link'
+
+    within all('.nested-fields').last do
+      fill_in 'Name', with: 'qna link'
+      fill_in 'Url', with: qna_url
+    end
+
+    click_on 'Save'
+
+    within('.question-links') do
+      expect(page).to have_link 'qna link', href: qna_url
+      expect(page).to have_link link.name, href: link.url
+    end
   end
 end
