53948554b6f2c43ac9e96f816d6b94ec383ebe03
Marina <rakastettu13@gmail.com>
2021-08-25 23:25:44 +0300

    the user can view the list of achievements

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -2,8 +2,4 @@ class AchievementsController < ApplicationController
   before_action :authenticate_user!, only: :received
 
   expose :achievements, -> { Achievement.all }
-
-  def received
-    render current_user.achievements
-  end
 end
