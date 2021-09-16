944433e25d1d09d2f926a6be8ec249a3b345d4b3
Marina <rakastettu13@gmail.com>
2021-09-16 06:57:21 +0300

    respond_to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-    render json: exception.message, status: :forbidden
+    respond_to do |format|
+      format.js { head :forbidden }
+      format.json { render json: exception.message, status: :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+    end
   end
 
   check_authorization unless: :devise_controller?
