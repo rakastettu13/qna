a8d6e70c0b8485ff3dae3cc37f295e54e4784c64
Marina <rakastettu13@gmail.com>
2021-09-21 07:59:29 +0300

    api profiles

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -23,4 +23,30 @@ describe 'Profiles API', type: :request do
       end
     end
   end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'when access_token is valid' do
+      let(:users) { create_list(:user, 3) }
+      let(:access_token) { create(:access_token, resource_owner_id: users.first.id).token }
+
+      before { get api_path, params: { access_token: access_token }, headers: headers }
+
+      it { expect(response).to be_successful }
+
+      it 'returns list of users without current user' do
+        expect(json['users'].size).to eq 2
+      end
+
+      include_examples 'user fields' do
+        let(:user) { users.last }
+        let(:user_json) { json['users'].last }
+      end
+    end
+  end
 end
